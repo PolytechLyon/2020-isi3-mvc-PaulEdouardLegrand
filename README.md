196e0d35938ca3317466ec194f02ff2237f6955f
PaulEdouardLegrand <45990151+PaulEdouardLegrand@users.noreply.github.com>
2020-05-29 09:34:12 +0200

    tp fonctionnel et Rapport complété

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ISI3 - MVP design pattern - "Game of Life"
 
-> Le rapport est à fournir dans ce document sous chacune des questions. 
+> Le rapport est à fournir dans ce document sous chacune des questions.
 > **Ne restez pas bloqués bêtement, demander de l'aide**
 > Ne copier pas le code de votre voisin, ça se voit.
 
-Nom/Prénom: `.......`
+Nom/Prénom: `Legrand/Paul`
 
-Lien du codesandbox: `.......`
+Lien du codesandbox: `https://codesandbox.io/s/divine-cdn-hj095`
 
 > Pour générer un codesandbox associé à votre code, [suiver cette doc](https://codesandbox.io/docs/importing#import-from-github)
 
@@ -17,16 +17,18 @@ Il est inventé par [John Horton Conway](https://fr.wikipedia.org/wiki/John_Hort
 
 ## Avant-propos
 
-1. Expliquer le design pattern MVC à l'aide d'un schéma à insérer directement ici. 
-Utiliser un outils commde Dia pour le représenter. Je veux **votre** schéma, pas un de ceux qu'on peut trouver sur le net.
+1. Expliquer le design pattern MVC à l'aide d'un schéma à insérer directement ici.
+   Utiliser un outils commde Dia pour le représenter. Je veux **votre** schéma, pas un de ceux qu'on peut trouver sur le net.
+   ![](./mvc.png)
 
-2. Expliquer ce pattern à l'aide en complétant ce texte.
+2) Expliquer ce pattern à l'aide en complétant ce texte.
 
-Le pattern MVP, vise à découper le `.......`, de la `.......` et du `.......` afin de rendre le code plus `.......`.
-Les responsabilités ne sont alors plus `.......`.
-On peut ainsi changer l'aspect visuel de sont application sans pour autant impacter le `.......`.
+Le pattern MVP, vise à découper le `modèle`, de la `vue` et du `controlleur` afin de rendre le code plus `dynamique`.
+Les responsabilités ne sont alors plus `groupées en une seule classe`.
+On peut ainsi changer l'aspect visuel de sont application sans pour autant impacter le `modèle`.
 
 3. Expliquer dans quels cas on doit privilégier le pattern MVC.
+   Le pattern mvc est principalement utilisé pour toutes les applications nécessitant une interface graphique dont les changements sont assez courants par exemple un site internet ou un jeu téléphone.
 
 ## A faire (obligatoire)
 
@@ -42,10 +44,11 @@ En quoi cela relève du design pattern ObserverObservable.
 
 1. Expliquer votre implémentation:
 
-L'usage d'une callback permet ici de `.......` afin dire à la _View_ de se redessiner.
-L'objet _Model_ n'a pas de lien avec `.......` pourtant grâce à la `.......` il peut notifier la `.......`.
+L'usage d'une callback permet ici de `notifier` afin dire à la _View_ de se redessiner.
+L'objet _Model_ n'a pas de lien avec `l'affichage` pourtant grâce à la `méthode update` il peut notifier la `_View_`.
 
 2. Insérer ici un UML montrant le pattern Observer-Observable liés aux objects de ce TP.
+   ![](./ObserverObservable.png)
 
 ## Optionel
 
--- src/gameOfLife/model.js
+++ src/gameOfLife/model.js
@@ -6,10 +6,12 @@ import {
 } from "./constants";
 
 export class Model {
-  constructor() {
+  constructor(callback) {
+    this.callback = callback;
     this.width = GAME_SIZE;
     this.height = GAME_SIZE;
     this.raf = null;
+    this.observers = [];
   }
 
   init() {
@@ -26,14 +28,30 @@ export class Model {
     this.raf = requestAnimationFrame(() => {
       const currentTime = new Date().getTime();
       if (currentTime - date > RENDER_INTERVAL) {
-
+        let nextState = Array.from(new Array(this.height), () =>
+          Array.from(new Array(this.width), () => CELL_STATES.NONE)
+        );
+        for (let i = 0; i < this.width; i++) {
+          for (let j = 0; j < this.width; j++) {
+            nextState[i][j] = this.state[i][j];
+          }
+        }
         for (let i = 0; i < this.width; i++) {
           for (let j = 0; j < this.width; j++) {
             const nbAlive = this.aliveNeighbours(i, j);
             // TODO implement Game of life logic
+            if (
+              this.state[i][j] === CELL_STATES.ALIVE &&
+              nbAlive !== 2 &&
+              nbAlive !== 3
+            ) {
+              nextState[i][j] = CELL_STATES.DEAD;
+            } else if (nbAlive === 3) {
+              nextState[i][j] = CELL_STATES.ALIVE;
+            }
           }
         }
-
+        this.state = nextState;
         this.updated();
         this.run(currentTime);
       } else {
@@ -49,6 +67,9 @@ export class Model {
 
   reset() {
     // TODO
+    this.stop();
+    this.init();
+    this.updated();
   }
 
   isCellAlive(x, y) {
@@ -56,17 +77,60 @@ export class Model {
       y >= 0 &&
       y < this.height &&
       x < this.height &&
-      this.state[y][x] === CELL_STATES.ALIVE
+      this.state[x][y] === CELL_STATES.ALIVE
       ? 1
       : 0;
   }
   aliveNeighbours(x, y) {
     let number = 0;
     // TODO
+    if (this.isCellAlive(x, y + 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x - 1, y + 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y + 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x - 1, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y - 1)) {
+      number++;
+    }
+    if (this.isCellAlive(x + 1, y)) {
+      number++;
+    }
+    if (this.isCellAlive(x - 1, y)) {
+      number++;
+    }
     return number;
   }
 
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    const removeIndex = this.observers.findIndex(obs => {
+      return observer === obs;
+    });
+    if (removeIndex !== -1) {
+      this.observers = this.observers.slice(removeIndex, 1);
+    }
+  }
+
+  notify(data) {
+    if (this.observers.length > 0) {
+      this.observers.forEach(observer => observer.update(data));
+    }
+  }
   updated() {
     // TODO update the view
+    this.callback(this);
   }
 }
